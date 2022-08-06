5feab6f836dc303debb65c36c020ab0fbe246975
Cao Chi Hai <haicao2805@gmail.com>
2022-08-06 13:40:41 +0700

    rebuild

---
+++ out/data/productData.ts
@@ -0,0 +1,144 @@
+import { Product, ProductCategory } from '../../src/core/models/product';
+
+export const productData: Product[] = [
+    {
+        id: '1',
+        name: 'Bounce Turbo Pod 5000 Hơi Disposable ',
+        category: ProductCategory.PodOneTime,
+        price: 75000,
+        images: [
+            {
+                id: '1',
+                src: '/assets/images/bouce-turbo-sua-chua-dau-600x600.jpg',
+                alt: '',
+            },
+            {
+                id: '2',
+                src: '/assets/images/fizzy-max-3000-product-600x600.jpg',
+                alt: '',
+            },
+            {
+                id: '3',
+                src: '/assets/images/Lio-Boom-3000-hoi-1-300x300.jpg',
+                alt: '',
+            },
+            {
+                id: '4',
+                src: '/assets/images/lux-models-product-300x300.jpg',
+                alt: '',
+            },
+        ],
+
+        description:
+            'Bounce Turbo là thiết bị sử dụng một lần với thiết kế khác biệt so với phần còn lại của ngành công nghiệp vaping, được trang bị Mesh coil với công suất mạnh mẽ và hương vị trái cây phong phú đem lại trải nghiệm vaping hoàn hảo. Buồng chứa rất lớn mang đến 11ml tinh dầu hương vị thơm ngon, với việc trang bị pin có thể sạc lại giúp thiết bị có thể đem tới 5000 hơi sử dụng. Bên ngoài thiết bị được thiết kế đẹp mắt với những vết cắt kim cương kèm nhiều màu sắc thời trang giúp trải nghiệm vaping của bạn được vui vẻ hơn.',
+        features: ['Dung lượng pin lớn đem tới 3500 hơi', '22 Hương vị lựa chọn hoàn hảo', 'Mesh coil 1.0 đem lại trải nghiệm hương vị tốt'],
+        specifications: ['Kích thước: 100mm x 24,5mm', 'Chất liệu: Hợp kim nhôm & Nhựa PCTG', 'Dung lượng: 1400mAh'],
+        boxInformations: ['1 Pod / 1 Hộp', '10 Hộp / 1 Pack'],
+        tastes: ["Dâu Kiwi", "Dâu Chuối", "Việt Quất", "Mâm Xôi Dưa Hấu", "Kiwi Ổi Chanh Leo", "Dâu Xoài, Nho", "Bơ Chanh Leo", "Dưa Hấu Chanh", "Sinh Tố Chuối", "Sinh Tố Dưa Hấu", "Sinh Tố Xoài Đào", "Soda Chanh", "Soda Chanh Gừng", "Chanh Mojito", "Tăng Lực Táo"]
+    },
+    {
+        id: '2',
+        name: 'Bounce Turbo Pod 5000 Hơi Disposable ',
+        category: ProductCategory.PodOneTime,
+        price: 75000,
+        images: [
+            {
+                id: '1',
+                src: '/assets/images/fizzy-max-3000-product-600x600.jpg',
+                alt: '',
+            },
+            {
+                id: '2',
+                src: '/assets/images/bouce-turbo-sua-chua-dau-600x600.jpg',
+                alt: '',
+            },
+            
+            {
+                id: '3',
+                src: '/assets/images/Lio-Boom-3000-hoi-1-300x300.jpg',
+                alt: '',
+            },
+            {
+                id: '4',
+                src: '/assets/images/lux-models-product-300x300.jpg',
+                alt: '',
+            },
+        ],
+        description:
+            'Bounce Turbo là thiết bị sử dụng một lần với thiết kế khác biệt so với phần còn lại của ngành công nghiệp vaping, được trang bị Mesh coil với công suất mạnh mẽ và hương vị trái cây phong phú đem lại trải nghiệm vaping hoàn hảo. Buồng chứa rất lớn mang đến 11ml tinh dầu hương vị thơm ngon, với việc trang bị pin có thể sạc lại giúp thiết bị có thể đem tới 5000 hơi sử dụng. Bên ngoài thiết bị được thiết kế đẹp mắt với những vết cắt kim cương kèm nhiều màu sắc thời trang giúp trải nghiệm vaping của bạn được vui vẻ hơn.',
+        features: ['Dung lượng pin lớn đem tới 3500 hơi', '22 Hương vị lựa chọn hoàn hảo', 'Mesh coil 1.0 đem lại trải nghiệm hương vị tốt'],
+        specifications: ['Kích thước: 100mm x 24,5mm', 'Chất liệu: Hợp kim nhôm & Nhựa PCTG', 'Dung lượng: 1400mAh'],
+        boxInformations: ['1 Pod / 1 Hộp', '10 Hộp / 1 Pack'],
+        tastes: ["Dâu Kiwi", "Dâu Chuối", "Việt Quất", "Mâm Xôi Dưa Hấu", "Kiwi Ổi Chanh Leo", "Dâu Xoài, Nho", "Bơ Chanh Leo", "Dưa Hấu Chanh", "Sinh Tố Chuối", "Sinh Tố Dưa Hấu", "Sinh Tố Xoài Đào", "Soda Chanh", "Soda Chanh Gừng", "Chanh Mojito", "Tăng Lực Táo"]
+    },
+    {
+        id: '3',
+        name: 'Bounce Turbo Pod 5000 Hơi Disposable ',
+        category: ProductCategory.PodOneTime,
+        price: 75000,
+        images: [
+            {
+                id: '3',
+                src: '/assets/images/Lio-Boom-3000-hoi-1-300x300.jpg',
+                alt: '',
+            },
+            {
+                id: '1',
+                src: '/assets/images/fizzy-max-3000-product-600x600.jpg',
+                alt: '',
+            },
+            {
+                id: '2',
+                src: '/assets/images/bouce-turbo-sua-chua-dau-600x600.jpg',
+                alt: '',
+            },
+            {
+                id: '4',
+                src: '/assets/images/lux-models-product-300x300.jpg',
+                alt: '',
+            },
+        ],
+        description:
+            'Bounce Turbo là thiết bị sử dụng một lần với thiết kế khác biệt so với phần còn lại của ngành công nghiệp vaping, được trang bị Mesh coil với công suất mạnh mẽ và hương vị trái cây phong phú đem lại trải nghiệm vaping hoàn hảo. Buồng chứa rất lớn mang đến 11ml tinh dầu hương vị thơm ngon, với việc trang bị pin có thể sạc lại giúp thiết bị có thể đem tới 5000 hơi sử dụng. Bên ngoài thiết bị được thiết kế đẹp mắt với những vết cắt kim cương kèm nhiều màu sắc thời trang giúp trải nghiệm vaping của bạn được vui vẻ hơn.',
+        features: ['Dung lượng pin lớn đem tới 3500 hơi', '22 Hương vị lựa chọn hoàn hảo', 'Mesh coil 1.0 đem lại trải nghiệm hương vị tốt'],
+        specifications: ['Kích thước: 100mm x 24,5mm', 'Chất liệu: Hợp kim nhôm & Nhựa PCTG', 'Dung lượng: 1400mAh'],
+        boxInformations: ['1 Pod / 1 Hộp', '10 Hộp / 1 Pack'],
+        tastes: ["Dâu Kiwi", "Dâu Chuối", "Việt Quất", "Mâm Xôi Dưa Hấu", "Kiwi Ổi Chanh Leo", "Dâu Xoài, Nho", "Bơ Chanh Leo", "Dưa Hấu Chanh", "Sinh Tố Chuối", "Sinh Tố Dưa Hấu", "Sinh Tố Xoài Đào", "Soda Chanh", "Soda Chanh Gừng", "Chanh Mojito", "Tăng Lực Táo"]
+    },
+    {
+        id: '4',
+        name: 'Bounce Turbo Pod 5000 Hơi Disposable ',
+        category: ProductCategory.PodOneTime,
+        price: 75000,
+        images: [
+            {
+                id: '1',
+                src: '/assets/images/fizzy-max-3000-product-600x600.jpg',
+                alt: '',
+            },
+            {
+                id: '2',
+                src: '/assets/images/bouce-turbo-sua-chua-dau-600x600.jpg',
+                alt: '',
+            },
+            
+            {
+                id: '3',
+                src: '/assets/images/Lio-Boom-3000-hoi-1-300x300.jpg',
+                alt: '',
+            },
+            {
+                id: '4',
+                src: '/assets/images/lux-models-product-300x300.jpg',
+                alt: '',
+            },
+        ],
+
+        description:
+            'Bounce Turbo là thiết bị sử dụng một lần với thiết kế khác biệt so với phần còn lại của ngành công nghiệp vaping, được trang bị Mesh coil với công suất mạnh mẽ và hương vị trái cây phong phú đem lại trải nghiệm vaping hoàn hảo. Buồng chứa rất lớn mang đến 11ml tinh dầu hương vị thơm ngon, với việc trang bị pin có thể sạc lại giúp thiết bị có thể đem tới 5000 hơi sử dụng. Bên ngoài thiết bị được thiết kế đẹp mắt với những vết cắt kim cương kèm nhiều màu sắc thời trang giúp trải nghiệm vaping của bạn được vui vẻ hơn.',
+        features: ['Dung lượng pin lớn đem tới 3500 hơi', '22 Hương vị lựa chọn hoàn hảo', 'Mesh coil 1.0 đem lại trải nghiệm hương vị tốt'],
+        specifications: ['Kích thước: 100mm x 24,5mm', 'Chất liệu: Hợp kim nhôm & Nhựa PCTG', 'Dung lượng: 1400mAh'],
+        boxInformations: ['1 Pod / 1 Hộp', '10 Hộp / 1 Pack'],
+        tastes: ["Dâu Kiwi", "Dâu Chuối", "Việt Quất", "Mâm Xôi Dưa Hấu", "Kiwi Ổi Chanh Leo", "Dâu Xoài, Nho", "Bơ Chanh Leo", "Dưa Hấu Chanh", "Sinh Tố Chuối", "Sinh Tố Dưa Hấu", "Sinh Tố Xoài Đào", "Soda Chanh", "Soda Chanh Gừng", "Chanh Mojito", "Tăng Lực Táo"]
+    },
+];
