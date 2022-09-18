0f91311f323d3f24d92eceda88aad3d107edc762
Cao Chi Hai <haicao2805@gmail.com>
2022-09-19 00:38:36 +0700

    export

---
+++ out/data/productData.ts
@@ -0,0 +1,186 @@
+import { Product, ProductCategory } from '../../src/core/models/product';
+
+export const productData: Product[] = [
+    {
+        id: '1',
+        name: 'Vozol Alien 7 Pod 2500 Hơi – Pod Dùng 1 Lần',
+        category: ProductCategory.PodOneTime,
+        price: 230000,
+        images: [
+            {
+                id: '1',
+                src: '/assets/product1/01.jpg',
+                alt: '',
+            },
+            {
+                id: '2',
+                src: '/assets/product1/02.jpg',
+                alt: '',
+            },
+            {
+                id: '3',
+                src: '/assets/product1/03.jpg',
+                alt: '',
+            },
+            {
+                id: '4',
+                src: '/assets/product1/04.jpg',
+                alt: '',
+            },
+            {
+                id: '5',
+                src: '/assets/product1/05.jpg',
+                alt: '',
+            },
+            {
+                id: '6',
+                src: '/assets/product1/06.jpg',
+                alt: '',
+            },
+            {
+                id: '7',
+                src: '/assets/product1/07.jpg',
+                alt: '',
+            },
+            {
+                id: '8',
+                src: '/assets/product1/08.jpg',
+                alt: '',
+            },
+            {
+                id: '9',
+                src: '/assets/product1/09.jpg',
+                alt: '',
+            },
+            {
+                id: '10',
+                src: '/assets/product1/10.jpg',
+                alt: '',
+            },
+        ],
+        description:"Vozol Alien 7 là sản phẩm pod sử dụng một lần mới nhất của vozol, với thiết kế phong cách thời trang kèm với màu sắc rực rỡ vẻ bề ngoài sáng bóng như pha lê. Vozol Alien 7 được trang bị công nghệ độc quyền kiểm soát cân bằng tốt cho tốc độ lấy dầu vào dây đốt nóng chỉ trong tích tắc. Nó có hiệu quả tránh cháy khô và cải thiện tuổi thọ làm việc của pin lên tới 200%. Alien 7 có hộp đựng tinh dầu độc lập giúp ngăn chặn việc leak tinh dầu an toàn và lành mạnh hơn cho người dùng, với việc thiết kế ngăn chứa độc lập làm ngăn chặn bay hơi và ngưng tụ tinh dầu trong miệng giúp trải nghiệm vaping trở lên hoàn hảo hơn. Thiết bị không có bất kỳ nút bấm nào và sử dụng cơ chế kích hoạt tự động để vaping nhanh chóng và dễ dàng. Không cần đổ đầy tinh dầu việc đơn giản của chỉ cần hút vào và bỏ đi khi nó hết pin.",
+        features: [],
+        specifications: ['Dung lượng pin: 1100mAh', 'Hàm lượng: 50mg', 'Puff: 2500 hơi', 'Dung tích: 7ml'],
+        boxInformation: ['1 Pod / 1 Hộp', '10 Pod / 1 Pack'],
+        tastes: ["Dâu Lạnh",
+            "Nho Lạnh",
+            "Dứa Dừa",
+            "Táo",
+            "Dâu Dưa Hấu",
+            "Bạc Hà",
+            "Xoài Lạnh",
+            "Chuối Lạnh",
+            "Dâu Việt Quất",
+            "Đào Lạnh",
+            "Vải Lạnh",
+            "Ổi Lạnh",]
+    },
+    {
+        id: '2',
+        name: 'Vozol Alien 5000 Hơi Disposable – Pod 1 Lần',
+        category: ProductCategory.PodOneTime,
+        price: 280000,
+        images: [
+            {
+                id: '1',
+                src: '/assets/product2/01.jpg',
+                alt: '',
+            },
+            {
+                id: '2',
+                src: '/assets/product2/02.jpg',
+                alt: '',
+            },
+            {
+                id: '3',
+                src: '/assets/product2/03.jpg',
+                alt: '',
+            },
+            {
+                id: '4',
+                src: '/assets/product2/04.jpg',
+                alt: '',
+            },
+            {
+                id: '5',
+                src: '/assets/product2/05.jpg',
+                alt: '',
+            },
+            {
+                id: '6',
+                src: '/assets/product2/06.jpg',
+                alt: '',
+            },
+            {
+                id: '7',
+                src: '/assets/product2/07.jpg',
+                alt: '',
+            },
+            {
+                id: '8',
+                src: '/assets/product2/08.jpg',
+                alt: '',
+            },
+            {
+                id: '9',
+                src: '/assets/product2/09.jpg',
+                alt: '',
+            },
+            {
+                id: '10',
+                src: '/assets/product2/10.jpg',
+                alt: '',
+            },
+            {
+                id: '11',
+                src: '/assets/product2/11.jpg',
+                alt: '',
+            },
+            {
+                id: '12',
+                src: '/assets/product2/12.jpg',
+                alt: '',
+            },
+            {
+                id: '13',
+                src: '/assets/product2/13.jpg',
+                alt: '',
+            },
+            {
+                id: '14',
+                src: '/assets/product2/14.jpg',
+                alt: '',
+            },
+            {
+                id: '15',
+                src: '/assets/product2/15.jpg',
+                alt: '',
+            },
+            {
+                id: '16',
+                src: '/assets/product2/16.jpg',
+                alt: '',
+            },
+            {
+                id: '17',
+                src: '/assets/product2/17.jpg',
+                alt: '',
+            },
+            {
+                id: '18',
+                src: '/assets/product2/18.jpg',
+                alt: '',
+            },
+            {
+                id: '19',
+                src: '/assets/product2/19.jpg',
+                alt: '',
+            },
+        ],
+        description:"Vozol Alien 5000 Hơi là sản phẩm mới nhất vừa được Vozol cho ra mắt, với thiết kế những vết cắt kim cương được thừa kế từ dòng sản phẩm Vozol alien 7. Với việc được làm mới hoàn toàn hương vị giúp thiết bị được cải thiện hơn so với phiên bản trước, sử dụng một viên pin 500mAh được trang bị sạc nhanh vô cùng tiện lợi giúp thiết bị có thể kéo dài tới 5000 hơi sử dụng. Nồng độ nicotine được dữ nguyên ở mức 50mg một mức độ vừa phải thỏa mãn mọi người dùng với buồng chứa 14ml giúp quá trình vaping có thể tận hưởng hương vị tới hơi cuối cùng.",
+        features: [],
+        specifications: ['Số Hơi : 5000 Puff', 'Dung Tích : 14ml', 'Dung Lượng : 500mAh (hỗ trợ sạc lại)', 'Hàm Lượng : 50mg'],
+        boxInformation: ['1 Pod / 1 Hộp', '10 Pod / 1 Pack'],
+        tastes: ["Dâu Kiwi", "Dâu Chuối", "Việt Quất", "Mâm Xôi Dưa Hấu", "Kiwi Ổi Chanh Leo", "Dâu Xoài", "Nho", "Bơ Chanh Leo", "Dưa Hấu Chanh", "Sinh Tố Chuối", "Sinh Tố Dưa Hấu", "Sinh Tố Xoài Đào", "Soda Chanh", "Soda Chanh Gừng", "Mojito Chanh", "Tăng Lực Táo"]
+    }
+];
